980fab8d5f0678bd64b3f9f94b81a81606b2161e
DarkNeoHQ <30746207+DarkNeoHQ@users.noreply.github.com>
2018-02-10 23:09:04 +0800

    1

--- em_2/em_2.py
+++ em_2/em_2.py
@@ -1,25 +1,31 @@
 import tensorflow as tf
 import numpy as np
-#产生数据
-x_data=np.random.rand(100).astype(np.float32)#随机的方式产生x
-y_data=x_data*5+2
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
 
-#设置权重,偏置
-Weights=tf.Variable(tf.random_uniform([1],-1.0,1.0))
-biases=tf.Variable(tf.zeros([1]))
-
-y=Weights*x_data+biases
-
-loss=tf.reduce_mean(tf.square(y-y_data))
-optimizer=tf.train.GradientDescentOptimizer(0.5)
-train=optimizer.minimize(loss)
-
-init=tf.initialize_all_variables()
-###激活
-sess=tf.Session()
+    return outputs
+x_data = np.linspace(-1,1,300, dtype=np.float32)[:,None]
+noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
+y_data = np.square(x_data) - 0.5 + noise
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_function=None)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+init = tf.global_variables_initializer()
+sess = tf.Session()
 sess.run(init)
-
-for step in range(300):
-    sess.run(train)
-    if step%20==0:
-        print(step,sess.run(Weights),sess.run(biases))
+for i in range(5000):
+    # training
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        # to see the step improvement
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
